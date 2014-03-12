dc06f8ee8a0886381c2f7d65708cc049cdc1c099
dyep49 <dyep49@gmail.com>
2014-03-12 13:40:41 -0400

    first commit0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularDecryptsy::Application.config.secret_key_base = 'fd64f62ddafb6613d000ea4dd89e279a629d270c4804bb386c08813e6bd39db436993700021bc63ebbb3d0060ba91c453a0f1c1e2a4d8c472f5dbd2ba37b7e68'
