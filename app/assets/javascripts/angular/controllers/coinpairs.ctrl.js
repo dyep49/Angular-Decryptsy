6e978cf7c8c695f1d543752f99dca3884e44b274
dyep49 <dyep49@gmail.com>
2014-03-26 01:51:46 -0400

    sortable tables

--- app/assets/javascripts/angular/controllers/coinpairs.ctrl.js
+++ app/assets/javascripts/angular/controllers/coinpairs.ctrl.js
@@ -1,6 +1,9 @@
 app.controller('CoinpairsCtrl', ['$scope', 'Coinpair', '$location', function($scope, Coinpair, $location) {
   $scope.coinpairs = Coinpair.all();
 
+  $scope.orderByField = 'doubleWall';
+  $scope.reverseSort = false;
+
   $scope.showCoinpair = function(id){
   	console.log('clicked')
   	$location.path('/coinpair/' + id)
