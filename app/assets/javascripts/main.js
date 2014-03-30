eb8bafb0b31daa76cbce8f12b545433dfd5347f6
dyep49 <dyep49@gmail.com>
2014-03-30 18:52:37 -0400

    heroku settings

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,7 +3,7 @@ var app = angular.module("Decryptsy", ['ngResource', 'ngRoute', 'd3']);
 app.config(function($routeProvider){
 	$routeProvider
 		.when('/',{
-			templateUrl : 'templates/home.html.erb',
+			templateUrl : 'templates/home.html',
 			controller  : 'CoinpairsCtrl'
 		})
 		.when('/arbitrage',{
